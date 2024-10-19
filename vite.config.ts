2f3db4b2f1acad2d8dfdffca1fc1e01a4a3b0e3d
sheryar kayani <sheryarali2010@gmail.com>
2024-10-20 01:25:47 +0500

    v1 updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': process.env, // this ensures Vite can handle environment variables
-  },
   build: {
     outDir: 'dist',
   },
 });
-
